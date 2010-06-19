b476df77d4455a19bca5d2ac06f5a58374949346
Hanno Schlichting <hanno@hannosch.eu>
2010-06-19 17:48:29 +0000

    Boilerplate

---
+++ README.txt
@@ -0,0 +1,5 @@
+Overview
+========
+
+This distribution contains a full text indexing facility for Zope 2 and more
+specifically for Products.ZCatalog.
---
+++ setup.py
@@ -0,0 +1,42 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from os.path import join
+from setuptools import setup, find_packages, Extension
+
+setup(name='Products.ZCTextIndex',
+      version = '2.13.0dev',
+      url='http://pypi.python.org/pypi/Products.ZCTextIndex',
+      license='ZPL 2.1',
+      description="Full text indexing for ZCatalog / Zope 2.",
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=open('README.txt').read() + '\n' +
+                       open('CHANGES.txt').read(),
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      ext_modules=[
+        Extension(
+              name='Products.ZCTextIndex.stopper',
+              sources=[join('src', 'Products', 'ZCTextIndex', 'stopper.c')]),
+        Extension(
+              name='Products.ZCTextIndex.okascore',
+              sources=[join('src', 'Products', 'ZCTextIndex', 'okascore.c')]),
+      ],
+      install_requires=[
+        'setuptools',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      )
