4cb4336bc21778600682be6c2850b4645851d65d
Hanno Schlichting <hanno@hannosch.eu>
2016-08-30 08:00:48 +0200

    vb [ci skip]

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Products.ZCTextIndex',
-      version='4.0.1',
+      version='4.0.2.dev0',
       url='https://github.com/zopefoundation/Products.ZCTextIndex',
       license='ZPL 2.1',
       description='Obsolete distribution, use Products.ZCatalog.',
